a8a20e09108ecfc4921a4b409af537a159982672
Charlotte Koch <dressupgeekout@gmail.com>
2022-05-13 14:48:17 -0700

    Update AUX_DATA

--- __charlotte_build.sh
+++ __charlotte_build.sh
@@ -2,5 +2,5 @@
 set -ex
 test -d Build || mkdir -p Build
 cd Build
-cmake ../Projects -DCMAKE_BUILD_TYPE=Release -DENABLE_RAD_TELEMETRY=OFF -DBUILD_OGDA=OFF -DAUX_DATA=/home/charlotte/games/overgrowth
+cmake ../Projects -DCMAKE_BUILD_TYPE=Release -DENABLE_RAD_TELEMETRY=OFF -DBUILD_OGDA=OFF -DAUX_DATA=/usr/pkg/share/overgrowth
 make -j4
